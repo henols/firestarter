8e437963face7621c01ac97ccbdf074177eddcf4
aceone <henols@gmail.com>
2025-01-10 09:01:35 +0100

    Read fixes

--- include/utils.h
+++ include/utils.h
@@ -7,9 +7,11 @@ extern "C" {
 #endif
 
 int execute_function(void (*callback)(firestarter_handle_t* handle), firestarter_handle_t* handle);
+
 int check_response(firestarter_handle_t* handle);
 
 void reset_timeout();
+
 int wait_for_ok(firestarter_handle_t* handle);
 
 #ifdef __cplusplus
