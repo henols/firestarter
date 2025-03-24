5600eda6b527663a6ffb496831873df277f394f3
aceone <henols@gmail.com>
2025-03-24 19:48:40 +0100

    Extend buffer

--- include/firestarter.h
+++ include/firestarter.h
@@ -8,8 +8,9 @@
 #ifndef FIRESTARTER_H
 #define FIRESTARTER_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+
 #include "rurp_shield.h"
 
 #define FW_VERSION VERSION ":" RURP_BOARD_NAME
@@ -17,7 +18,7 @@
 #ifndef DATA_BUFFER_SIZE
 #define DATA_BUFFER_SIZE 512
 #endif
-#define RESPONSE_MSG_SIZE 64
+#define RESPONSE_MSG_SIZE 96
 
 #define STATE_IDLE 0
 #define STATE_READ 1
@@ -29,11 +30,11 @@
 
 #ifdef DEV_TOOLS
 #define STATE_DEV_ADDRESS 7
-#define STATE_DEV_REGISTER 8 
+#define STATE_DEV_REGISTER 8
 #endif
 
 #define STATE_READ_VPP 11
-#define STATE_READ_VPE 12 
+#define STATE_READ_VPE 12
 #define STATE_FW_VERSION 13
 #define STATE_CONFIG 14
 #define STATE_HW_VERSION 15
@@ -53,69 +54,67 @@
 #define FLAG_CHIP_ENABLE 0x40
 
 #define firestarter_warning_response(msg) \
-  firestarter_set_responce(RESPONSE_CODE_WARNING, msg)
+    firestarter_set_responce(RESPONSE_CODE_WARNING, msg)
 
-#define firestarter_warning_response_format( msg, ...) \
- firestarter_response_format(RESPONSE_CODE_WARNING, msg, __VA_ARGS__)
+#define firestarter_warning_response_format(msg, ...) \
+    firestarter_response_format(RESPONSE_CODE_WARNING, msg, __VA_ARGS__)
 
 #define firestarter_error_response(msg) \
-  firestarter_set_responce(RESPONSE_CODE_ERROR,msg)
+    firestarter_set_responce(RESPONSE_CODE_ERROR, msg)
 
-#define firestarter_error_response_format( msg, ...) \
- firestarter_response_format(RESPONSE_CODE_ERROR, msg, __VA_ARGS__)
+#define firestarter_error_response_format(msg, ...) \
+    firestarter_response_format(RESPONSE_CODE_ERROR, msg, __VA_ARGS__)
 
-#define firestarter_set_responce( code, msg) \
-  copy_to_buffer(handle->response_msg, msg); \
-  handle->response_code = code;
+#define firestarter_set_responce(code, msg)    \
+    copy_to_buffer(handle->response_msg, msg); \
+    handle->response_code = code;
 
 #define firestarter_response_format(code, msg, ...) \
-  {format(handle->response_msg, msg, __VA_ARGS__);} \
-  handle->response_code = code;
+    format(handle->response_msg, msg, __VA_ARGS__); \
+    handle->response_code = code;
 
 #define is_flag_set(flag) \
-	((handle->ctrl_flags & flag) == flag)
+    ((handle->ctrl_flags & flag) == flag)
 
 typedef struct bus_config {
-	uint8_t address_lines[19]; // Array mapping address lines
-	uint32_t address_mask;      // Mask for address lines
-	uint8_t matching_lines;	 // Number of matching address lines
-	uint8_t rw_line;           // RW line mapping
-	uint8_t vpp_line;		  // VPP line mapping
+    uint8_t address_lines[19];  // Array mapping address lines
+    uint32_t address_mask;      // Mask for address lines
+    uint8_t matching_lines;     // Number of matching address lines
+    uint8_t rw_line;            // RW line mapping
+    uint8_t vpp_line;           // VPP line mapping
 } bus_config_t;
 
-
 typedef struct firestarter_handle {
-	uint8_t verbose;
-	uint8_t state;
-	uint8_t init;
-	uint8_t response_code;
-	char response_msg[RESPONSE_MSG_SIZE];
-	uint8_t mem_type;
-	uint32_t protocol;
-	uint8_t pins;
-	uint32_t mem_size;
-	uint32_t address;
-	float vpp;
-	uint32_t pulse_delay;
-	uint32_t ctrl_flags;
-	uint16_t chip_id;
-	char data_buffer[DATA_BUFFER_SIZE];
-	uint32_t data_size;
-	bus_config_t bus_config;
+    uint8_t verbose;
+    uint8_t state;
+    uint8_t init;
+    uint8_t response_code;
+    char response_msg[RESPONSE_MSG_SIZE];
+    uint8_t mem_type;
+    uint32_t protocol;
+    uint8_t pins;
+    uint32_t mem_size;
+    uint32_t address;
+    float vpp;
+    uint32_t pulse_delay;
+    uint32_t ctrl_flags;
+    uint16_t chip_id;
+    char data_buffer[DATA_BUFFER_SIZE];
+    uint32_t data_size;
+    bus_config_t bus_config;
 
-	void (*firestarter_operation_init)(struct firestarter_handle*);
-	void (*firestarter_operation_execute)(struct firestarter_handle*);
-	void (*firestarter_operation_end)(struct firestarter_handle*);
+    void (*firestarter_operation_init)(struct firestarter_handle*);
+    void (*firestarter_operation_execute)(struct firestarter_handle*);
+    void (*firestarter_operation_end)(struct firestarter_handle*);
 
     void (*firestarter_set_data)(struct firestarter_handle*, uint32_t, uint8_t);
-	uint8_t(*firestarter_get_data)(struct firestarter_handle*, uint32_t);
-			
-	void (*firestarter_set_address)(struct firestarter_handle*, uint32_t);
-	
-	void (*firestarter_set_control_register)(struct firestarter_handle*, rurp_register_t, bool);
-	bool (*firestarter_get_control_register)(struct firestarter_handle*, rurp_register_t);
+    uint8_t (*firestarter_get_data)(struct firestarter_handle*, uint32_t);
+
+    void (*firestarter_set_address)(struct firestarter_handle*, uint32_t);
+
+    void (*firestarter_set_control_register)(struct firestarter_handle*, rurp_register_t, bool);
+    bool (*firestarter_get_control_register)(struct firestarter_handle*, rurp_register_t);
 
 } firestarter_handle_t;
 
-
-#endif // FIRESTARTER_H
+#endif  // FIRESTARTER_H
