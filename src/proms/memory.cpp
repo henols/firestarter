7602e6e34587d52389dfd9bd534b50f3b333bb6b
aceone <henols@gmail.com>
2025-07-12 15:32:09 +0200

    Compile error

--- src/proms/memory.cpp
+++ src/proms/memory.cpp
@@ -229,17 +229,17 @@ void mem_util_blank_check(firestarter_handle_t* handle) {
     blank_check_progress_data_t* progress_data;
     if (!is_operation_in_progress(handle)) {
         set_operation_in_progress(handle);
-        handle->proggress_data = malloc(sizeof(blank_check_progress_data_t));
-        progress_data = (blank_check_progress_data_t*)handle->proggress_data;
+        handle->progress_data = malloc(sizeof(blank_check_progress_data_t));
+        progress_data = (blank_check_progress_data_t*)handle->progress_data;
         progress_data->address = handle->address;
         handle->address = 0;
     } else {
-        progress_data = (blank_check_progress_data_t*)handle->proggress_data;
+        progress_data = (blank_check_progress_data_t*)handle->progress_data;
         if (handle->address >= handle->mem_size) {
             clear_operation_in_progress(handle);
             handle->address = progress_data->address;
-            free(handle->proggress_data);
-            handle->proggress_data = NULL;
+            free(handle->progress_data);
+            handle->progress_data = NULL;
             return;
         }
     }
