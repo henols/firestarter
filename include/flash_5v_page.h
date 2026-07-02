63e130ea7713aa9b5e77a73fc2354372eb640d7a
Henrik Olsson <henols@gmail.se>
2026-07-02 07:21:07 +0000

    feat(104-01): rename flash_type_4 -> flash_5v_page (0x05 + phantom handler)
    - git mv include/flash_type_4.h -> include/flash_5v_page.h
    - git mv src/proms/flash_type_4.cpp -> src/proms/flash_5v_page.cpp
    - fix misspelled/mismatched header guard -> __FLASH_5V_PAGE_H__ (was
      __FALSH__TYPE_4_H__ on #ifndef/#define vs __FLASH__TYPE_4_H__ on #endif)
    - rename public entry configure_flash4 -> configure_flash_5v_page
    - rename file-internal flash4_* static helpers -> flash_5v_page_* stem

--- include/flash_5v_page.h
+++ include/flash_5v_page.h
@@ -5,18 +5,18 @@
  * Permission is hereby granted under MIT license.
  */
 
-#ifndef __FALSH__TYPE_4_H__
-#define __FALSH__TYPE_4_H__
+#ifndef __FLASH_5V_PAGE_H__
+#define __FLASH_5V_PAGE_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 #include "firestarter.h"
 
-    void configure_flash4(firestarter_handle_t* handle);
+    void configure_flash_5v_page(firestarter_handle_t* handle);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __FLASH__TYPE_4_H__
+#endif // __FLASH_5V_PAGE_H__
