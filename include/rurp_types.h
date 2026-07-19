768580f22c0c2a25ba68bfc28447678e07ab7037
Henrik Olsson <henols@gmail.com>
2026-07-19 21:59:10 +0200

    Persist common VPP calibration in board configuration

--- include/rurp_types.h
+++ include/rurp_types.h
@@ -21,6 +21,18 @@ typedef struct rurp_configuration {
     long r1;
     long r2;
     uint8_t hardware_revision;
+
+    /* Common calibration of the complete divider + ADC measurement path.
+     * calibrated_mv = uncalibrated_mv * vpp_gain_ppm / 1,000,000
+     *               + vpp_offset_mv
+     * The original two points are retained so later firmware can recalculate
+     * coefficients if the representation changes. */
+    int32_t vpp_gain_ppm;
+    int32_t vpp_offset_mv;
+    uint16_t vpp_cal_measured_low_mv;
+    uint16_t vpp_cal_actual_low_mv;
+    uint16_t vpp_cal_measured_high_mv;
+    uint16_t vpp_cal_actual_high_mv;
 } rurp_configuration_t;
 
 #endif // __RURP_TYPES_H__
