1b6dadac0d501ce5e6a8050140add1e2beb6b189
Henrik Olsson <henols@gmail.com>
2026-07-20 10:09:04 +0200

    Add PY32F071 ARM GCC toolchain

---
+++ platform/py32f071/cmake/arm-none-eabi.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy CACHE INTERNAL "objcopy")
+set(CMAKE_SIZE arm-none-eabi-size CACHE INTERNAL "size")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
