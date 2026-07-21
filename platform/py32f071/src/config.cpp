d2f31558771f9f4174db24dcd5b80cb5460d5948
Henrik Olsson <henols@gmail.com>
2026-07-21 11:06:07 +0200

    Implement PY32F071 runtime configuration backend

---
+++ platform/py32f071/src/config.cpp
@@ -0,0 +1,37 @@
+#include <string.h>
+
+#include "rurp_shield.h"
+
+namespace
+{
+rurp_configuration_t configuration;
+}
+
+extern "C" rurp_configuration_t *rurp_get_config(void)
+{
+    return &configuration;
+}
+
+extern "C" void rurp_validate_config(rurp_configuration_t *value)
+{
+    if (strcmp(value->version, CONFIG_VERSION) != 0 || value->r2 == 0)
+    {
+        memset(value, 0, sizeof(*value));
+        strcpy(value->version, CONFIG_VERSION);
+        value->r1 = VALUE_R1;
+        value->r2 = VALUE_R2;
+        value->hardware_revision = 0xFFU;
+    }
+}
+
+extern "C" void rurp_load_config(void)
+{
+    memset(&configuration, 0, sizeof(configuration));
+    rurp_validate_config(&configuration);
+}
+
+extern "C" void rurp_save_config(rurp_configuration_t *value)
+{
+    rurp_validate_config(value);
+    configuration = *value;
+}
